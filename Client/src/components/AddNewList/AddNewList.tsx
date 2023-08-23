b62aa22e817590a9d04c1bfbf8ac15043f99b84d
Wild-Design <brenneke_ruger@hotmail.com>
2023-08-23 17:47:29 -0300

    mo

--- Client/src/components/AddNewList/AddNewList.tsx
+++ Client/src/components/AddNewList/AddNewList.tsx
@@ -1,38 +1,47 @@
 import styles from './AddNewList.module.css';
 import { IoMdAdd } from 'react-icons/io';
 import { RxCross1 } from 'react-icons/rx';
-import { FC, useState } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { getFullDataUser } from '../../features/userSlice';
 import { createList } from '../../utils/';
-
+import { ThreeDots } from 'react-loader-spinner';
 const AddNewList: FC = () => {
   const dispatch = useAppDispatch();
   const { fullData } = useAppSelector((state) => state.userSlice);
 
-  const [addInput, setAddInput] = useState(false);
-  const [captureText, setCaptureText] = useState('');
+  const [addInput, setAddInput] = useState(false); //Lo uso para ocultar el botton inicial y para mostrar el input y demas botones
+  const [inputValue, setInputValue] = useState(''); //Aquí solo tomo el valor del input
+  const [charging, setCharging] = useState(false); //Aquí solo agrego el spinner de carga y quito los botones de agregar y borrar para que no hallan posibles problemas
 
-  const handleInputChange = (event: any) => {
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
-    setCaptureText(value);
+    setInputValue(value);
   };
-  const addList = async () => {
+  const addList = async (): Promise<void> => {
     try {
-      await createList(
-        captureText,
-        fullData?.id!,
-        fullData?.user_name!,
-        fullData?.password!
-      );
-      await dispatch(
-        getFullDataUser(fullData?.user_name!, fullData?.password!)
-      );
-      setAddInput(false);
+      if (inputValue.trim().length > 0 && inputValue.length > 0) {
+        //compruevo que el input tenga al menos un caracter
+        setCharging(true);
+        await createList(
+          inputValue,
+          fullData?.id!,
+          fullData?.user_name!,
+          fullData?.password!
+        );
+        await dispatch(
+          getFullDataUser(fullData?.user_name!, fullData?.password!)
+        );
+        setAddInput(false);
+        setCharging(false);
+        setInputValue('');
+      }
     } catch (error: any) {
+      setCharging(false);
       alert('Algo salió mal, intenta guardar la lista mas tarde');
     }
   };
+
   return (
     <>
       <div
@@ -51,17 +60,31 @@ const AddNewList: FC = () => {
             type='text'
             placeholder='Introduzca el título de la lista...'
           />
-          <div className={styles.buttons}>
-            <button onClick={addList} className={styles.buttonAddList}>
-              Añadir lista
-            </button>
-            <div
-              onClick={() => setAddInput(false)}
-              className={styles.buttonCancel}
-            >
-              <RxCross1 />
+          {!charging ? (
+            <div className={styles.buttons}>
+              <button onClick={addList} className={styles.buttonAddList}>
+                Añadir lista
+              </button>
+              <div
+                onClick={() => setAddInput(false)}
+                className={styles.buttonCancel}
+              >
+                <RxCross1 />
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className={styles.spinner}>
+              <ThreeDots
+                height='30'
+                width='40'
+                radius='9'
+                color='#4fa94d'
+                ariaLabel='three-dots-loading'
+                wrapperStyle={{}}
+                visible={true}
+              />
+            </div>
+          )}
         </div>
       )}
     </>
