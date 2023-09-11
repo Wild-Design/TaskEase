5fa9621c02bec2520f83ceef3ba02f188435c0aa
Wild-Design <brenneke_ruger@hotmail.com>
2023-09-11 12:41:17 -0300

    s

--- Client/src/components/TableroDelCasino/TableroDelCasino.tsx
+++ Client/src/components/TableroDelCasino/TableroDelCasino.tsx
@@ -1,12 +1,13 @@
-let numeros: number[] = [
-  2301, 2302, 2303, 2304, 2305, 2306, 2307, 2308, 2309, 2310, 2311, 2312, 2313,
-  2314, 2315, 2316, 2317, 2318, 2319, 2320,
-];
+let numbers: number[] = [];
+
+for (let i = 551; i <= 600; i++) {
+  numbers.push(i);
+}
 
 const TableroDelCasino = () => {
   return (
     <div className='padre'>
-      {numeros.map((numero) => (
+      {numbers.map((numero) => (
         <div className='hijos'>{numero}</div>
       ))}
     </div>
